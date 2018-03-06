145846276dc0fed3a4670ad89b45e17670e2fd19
ziya <marmeladze@yandex.ru>
2018-03-06 19:02:57 +0400

    add cpu usage to instance view

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,7 +1,21 @@
 class Instance < ActiveRecord::Base
   has_many :instance_details
+  has_many :cpu_loads
 
   def detail
     instance_details.last
   end
+
+  def max_load_for_today
+  	cpu_loads.where("DATE(created_at) = DATE(?)", Time.now).maximum(:percentage)
+  end
+
+  def min_load_for_today
+  	cpu_loads.where("DATE(created_at) = DATE(?)", Time.now).minimum(:percentage)
+  end
+
+  def avg_load_for_today
+  	cpu_loads.where("DATE(created_at) = DATE(?)", Time.now).average(:percentage).to_f
+  end
+
 end
