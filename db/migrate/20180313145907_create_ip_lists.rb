70b8f3ab85baa7ddc901c79fcaa069d965ea0614
ziya <marmeladze@yandex.ru>
2018-03-14 00:05:46 +0400

    fix typo

--- db/migrate/20180313145907_create_ip_lists.rb
+++ db/migrate/20180313145907_create_ip_lists.rb
@@ -2,7 +2,7 @@ class CreateIpLists < ActiveRecord::Migration
   def change
     create_table :ip_lists do |t|
       t.string :address
-      t.string :type
+      t.string :list_type
       t.references :instance, index: true, foreign_key: true
 
       t.timestamps null: false
